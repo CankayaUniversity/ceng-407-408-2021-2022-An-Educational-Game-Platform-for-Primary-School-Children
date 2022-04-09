c28ff30dc54595a27785d7f63ee1f8225eeadb39
ziyacanerandac <68267338+ziyacanerandac@users.noreply.github.com>
2022-04-09 14:15:08 +0300

    Hotfix/fixed Timer
    There were conflicts due to 2 different timers with the same name but is fixed.

---
+++ Primath/Assets/Scripts/Timerkurt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timerkurt : MonoBehaviour
+{
+    public Text timerText;
+    public int CountTime;
+    string minutes, seconds;
+
+    void Start()
+    {
+        StartCoroutine(CountDown());
+    }
+
+    IEnumerator CountDown()
+    {
+        while (CountTime > 0)
+        {
+            UpdateTime();
+
+            yield return new WaitForSeconds(1);
+
+            CountTime--;
+        }
+    }
+
+    private void UpdateTime()
+    {
+        minutes = (CountTime / 60).ToString("00");
+        seconds = (CountTime % 60).ToString("00");
+        timerText.text = minutes + ":" + seconds;
+    }
+}
