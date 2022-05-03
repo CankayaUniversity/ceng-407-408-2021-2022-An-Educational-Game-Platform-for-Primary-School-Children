7964253c95b3292f2aad2074ffa363ca1b3b90f9
ziyacanerandac <68267338+ziyacanerandac@users.noreply.github.com>
2022-05-03 14:07:44 +0300

    feature/add_analog_to_digital_game_in_seconds_grade_games
    add new game to second grade games(analog to digital).

---
+++ Primath/Assets/Scripts/saat2.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class saat2 : MonoBehaviour
+{
+    public InputField input;
+    public InputField input2;
+    public float kalan;
+    public Text TXT;
+    public int istenilensaat;
+    public int istenilendakika;
+    public int number;
+    public int number2;
+    public GameObject kazanmaPanel;
+    public GameObject kaybetmepanali;
+    private bool flag = true;
+
+    void Start()
+    {
+        TXT.text = "KALAN SANİYE: " + kalan;
+    }
+
+    void Update()
+    {
+        if(kalan !=0 && flag == true)
+        kalan -= Time.deltaTime;
+
+        TXT.text = "KALAN SANİYE: " + Mathf.RoundToInt(kalan);
+        if (kalan == 0 || kalan < 0)
+        {
+            kaybetmepanali.SetActive(true);
+            flag = false;
+        }
+    }
+    public void GetInputSaat()
+    {
+        int.TryParse(input.text, out int result);
+        number = result;
+        Debug.Log("saat:" + number);
+    }
+    public void GetInputDakika()
+    {
+        int.TryParse(input2.text, out int result2);
+        number2 = result2;
+
+        Debug.Log("dakika:" + number2);
+        Results();
+
+    }
+    public void Results()
+    {
+        if (number == istenilensaat && number2 == istenilendakika && kalan > 0)
+        {
+
+            kazanmaPanel.SetActive(true);
+            flag = false;
+        }
+       
+        else
+            Debug.Log("Tekrar Dene");
+    }
+    // Start is called before the first frame update
+   
+    // Update is called once per frame
+   
+}
